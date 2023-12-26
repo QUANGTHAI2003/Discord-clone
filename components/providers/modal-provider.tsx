f1385fe70a8f37a1747890a539ab879f7037dbed
QuangThai_Dev <tranquangthai.10102003@gmail.com>
2023-12-26 16:58:37 +0700

    [API] - Delete & Leave server modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,8 +4,10 @@ import { useEffect, useState } from "react";
 
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 
 export const ModalProvider = () => {
@@ -22,6 +24,8 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
       <CreateChannelModal />
       <EditServerModal />
       <InviteModal />
